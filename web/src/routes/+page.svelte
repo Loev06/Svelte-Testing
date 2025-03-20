ca59af5fa7a5feeb308775fa9e5eadb6cee63219
Loev06 <105797818+Loev06@users.noreply.github.com>
2025-03-20 20:59:25 +0100

    Shared and local counters

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+    import { counter } from "./counter.svelte";
+
+    let localCount = $state(0);
+</script>
+
 <h1>Main page</h1>
 <a href="/test">Test page</a>
+
+<br />
+<br />
+
+<button
+    onclick={() => {
+        counter.count += 1;
+        localCount += 1;
+    }}>+1</button
+>
+<p>Global count: {counter.count}</p>
+<p>Local count: {localCount}</p>
--- web/src/routes/test/+page.svelte
+++ web/src/routes/test/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+    import { counter } from "../counter.svelte";
+</script>
+
 <h1>Test page</h1>
 <a href="/">Main page</a>
+<p>Global count: {counter.count}</p>
